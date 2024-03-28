564e7ada67fd5d81f637a99b62806fa3a2e8b3de
Vijay Thulasiraaman <vijay@energetics.ai>
2024-03-28 18:08:53 +0530

    hoihi

--- src/App.js
+++ src/App.js
@@ -1,21 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
-import { router } from './routes';
+import logo from "./logo.svg";
+import "./App.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { router } from "./routes";
 
 const responseFacebook = (response) => {
   console.log(response);
-}
-
+};
 
 function App() {
   return (
-    <div className="App">
-       <RouterProvider router={router} />
-    </div>
+    
+      <div className="App">
+        <RouterProvider router={router} />
+      </div>
+    // </React.StrictMode>
   );
 }
 
